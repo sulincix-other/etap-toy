5ff8ea36043566999512460199f1db8face1b9c0
aliriza <aliriza.keskin@pardus.org.tr>
2025-02-18 22:35:17 +0300

    move event

--- toy/MainWindow.py
+++ toy/MainWindow.py
@@ -3,6 +3,8 @@
 import gi
 import os
 
+import time
+
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk
 
@@ -30,20 +32,28 @@ class MainWindow(Gtk.Window):
         self.set_keep_above(True)
         self.set_skip_taskbar_hint(True)
         self.set_visual(self.get_screen().get_rgba_visual())
+        self._time = 0
 
         # Load CSS for transparency
         self.load_css()
 
         self.set_application(application)
+        but = Gtk.Button()
 
-        # Destroy signal
+        # Signals
         self.connect("destroy", Gtk.main_quit)
+        but.connect("motion-notify-event", self.on_motion_notify_event)
+        but.connect("button-release-event", self.on_release_event)
+        but.connect("button-press-event", self.on_press_event)
 
         # Add button
-        self.add(Gtk.Button())
+        self.add(but)
 
         # show main window
         self.show_all()
+
+    def do_popup_event(self, widget=None, event=None):
+        print("Hello World")
 
     def load_css(self):
         # Create a CSS provider and load the CSS data
@@ -59,3 +69,24 @@ class MainWindow(Gtk.Window):
             css_provider,
             Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
         )
+
+    def on_release_event(self, widget, event):
+        # Do event if clicked
+        if (time.time() * 1000) - self._time  < 150:
+            self.do_popup_event()
+
+    def on_press_event(self, widget, event):
+        self._time = time.time() * 1000
+
+    def on_motion_notify_event(self, widget, event):
+        if (event.state & Gdk.ModifierType.BUTTON1_MASK) == Gdk.ModifierType.BUTTON1_MASK:
+            # Get the allocation (size) of the window
+            allocation = widget.get_allocation()
+            w = allocation.width
+            h = allocation.height
+
+            # Move the window to the mouse position, centered
+            self.move(int(event.x_root - (w / 2)), int(event.y_root - (h / 2)))
+            self._time = 0
+            return True
+        return False
