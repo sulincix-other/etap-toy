929ede7261ff18ffcf1c56b2060acfb64cf782a6
aliriza <aliriza.keskin@pardus.org.tr>
2025-02-27 05:02:01 +0300

    pam auth functions added for lockscreen

--- src/toy.h
+++ src/toy.h
@@ -10,4 +10,5 @@ extern "C" {
     void ctx_init();
     void setShowMainWindow(bool state);
     void uinput_send(int type, int code, int value);
+    bool pam_auth(const char *username, const char *password);
 }
