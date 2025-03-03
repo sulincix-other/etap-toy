83581e2b8a07626a9da8e453e68f8cf253e0e84a
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2025-03-03 09:56:51 +0000

    gettext init fix

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,17 @@ extern "C" {
 static MainWindow *window;
 
 int main(int argc, char *argv[]) {
+    // translation part
+    const char *systemLanguage = getenv("LANG");
+    if (systemLanguage != nullptr) {
+        bindtextdomain("etap-toy", "/usr/share/locale");
+        setlocale(LC_ALL, systemLanguage);
+        textdomain("etap-toy");
+    } else {
+        fprintf(stderr, "WARNING: LANG environment variable not set.\n");
+    }
+
+
     setenv("QT_QPA_PLATFORM", "xcb;wayland",1);
     //Force ignore system dpi
     setenv("QT_AUTO_SCREEN_SCALE_FACTOR", "0", 1);
